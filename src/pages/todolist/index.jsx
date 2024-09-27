b63e5e88d1f65617143d48d7da57d3d21ba36c91
Arief Yusron <arief.yusron@efishery.com>
2024-09-27 20:04:01 +0700

    axios then

--- src/pages/todolist/index.jsx
+++ src/pages/todolist/index.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 import Button from '../../components/Button'
 import './styles.css'
 
 const App = () => {
   const [list, setList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
   const [input, setInput] = useState('')
   const navigate = useNavigate()
+
+  const getData = () => {
+    setIsLoading(true)
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos/')
+      .then((res) => {
+        console.log('success', res.data)
+        setList(res.data)
+        setIsLoading(false)
+      }).catch(() => {
+        setIsError(true)
+      })
+  }
+
+  useEffect(() => {
+    getData()
+  },[])
 
   return (
     <div>
@@ -32,27 +52,39 @@ const App = () => {
         Hapus Semua
       </Button>
 
-      <div className='wrap-content'>
-        {list.length === 0 ? <p>Kosong</p> : (
-          <ul>
-            {list.map((item, index) => {
-              return (
-                <li key={index} style={{ marginBottom: '12px' }}>
-                  {item}
-                  <Button
-                    style={{ marginLeft: '12px'}}
-                    onClick={() => {
-                      setList(list.filter((e, i) => i !== index ))
-                    }}
-                  >
-                    hapus
-                  </Button>
-                </li>
-              )
-            })}
-          </ul>
+      {isError ? (
+        <div>Error</div>
+      ) : (
+        <>
+          {isLoading ? (
+          <div>
+            <div>Loading</div>
+          </div>
+        ) : (
+          <div className='wrap-content'>
+            {list.length === 0 ? <p>Kosong</p> : (
+              <ul>
+                {list.map((item, index) => {
+                  return (
+                    <li key={index} style={{ marginBottom: '12px' }}>
+                      {item.title}
+                      <Button
+                        style={{ marginLeft: '12px'}}
+                        onClick={() => {
+                          setList(list.filter((e, i) => i !== index ))
+                        }}
+                      >
+                        hapus
+                      </Button>
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
         )}
-      </div>
+        </>
+      )}
     </div>
   )
 };
